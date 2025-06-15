52417c1b76a3e506acb24e908b944f71805ba575
abhim7022 <abhishekdindavar049@gmail.com>
2025-06-15 13:33:46 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required version= "> 1.2.0"
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+ami = "ami-830c94e3"
+instance_type = "t2.micro"
+tags = {
+name = "tera_ec"
+}
+}
+  
+  
